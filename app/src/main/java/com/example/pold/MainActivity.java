690afe6283a96a49b151e083db9b56fe5a1214a0
uurim <syl000126@naver.com>
2022-06-07 10:36:24 +0900

    first

---
+++ app/src/main/java/com/example/pold/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pold;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("폴디");
+    }
+}
